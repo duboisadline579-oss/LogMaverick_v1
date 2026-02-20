b615f8282377ea10d7a2526f66de10debb65191c
Duboisadline579-oss <Duboisadline579-oss@gmail.com>
2026-02-20 07:33:25 +0000

    update

--- Helpers/Converters.cs
+++ Helpers/Converters.cs
@@ -2,11 +2,22 @@ using System;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace LogMaverick.Helpers {
     public class CountToVisibilityConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
             return (value is int count && count > 0) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+    }
+
+    public class PauseColorConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            bool isPaused = (value is bool b) && b;
+            // 주황색(#CC4400) 또는 어두운 회색(#222222) 반환
+            string colorCode = isPaused ? "#CC4400" : "#222222";
+            return (SolidColorBrush)new ComponentModel.BrushConverter().ConvertFrom(colorCode);
         }
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
     }
