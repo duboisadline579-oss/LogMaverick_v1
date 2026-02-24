3553f5b96470028fdc337c144db725eee833696c
Duboisadline579-oss <Duboisadline579-oss@gmail.com>
2026-02-24 23:44:18 +0000

    update

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,14 @@
 using System.Windows;
+using LogMaverick.Helpers;
+
 namespace LogMaverick {
-    public partial class App : Application { }
+    public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            Resources["CountToVis"] = new CountToVisibilityConverter();
+            Resources["PauseColorConverter"] = new PauseColorConverter();
+            Resources["LogColorConverter"] = new LogTypeColorConverter();
+            Resources["FlashConverter"] = new NewLogFlashConverter();
+        }
+    }
 }
